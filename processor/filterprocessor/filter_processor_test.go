c2ec68730cbd79e6399457c5eca9128b953fa0bf
Przemek Maciolek <pmaciolek@sumologic.com>
2021-01-11 12:42:57 +0100

    Add span filtering capability

--- processor/filterprocessor/filter_processor_test.go
+++ processor/filterprocessor/filter_processor_test.go
@@ -22,13 +22,16 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/internal/goldendataset"
 	"go.opentelemetry.io/collector/internal/processor/filterconfig"
 	"go.opentelemetry.io/collector/internal/processor/filtermetric"
+	"go.opentelemetry.io/collector/internal/processor/filterset"
 	"go.opentelemetry.io/collector/model/pdata"
 )
 
@@ -149,13 +152,13 @@ var (
 			outMN: [][]string{{
 				"full_name_match",
 				"prefix/test/match",
-				// "prefix_test_match", excluded by exclude filter
+				// "prefix_test_match", excluded by excludeMetrics filter
 				"prefixprefix/test/match",
 				"test/match/suffix",
 				"test_match_suffix",
 				"test/match/suffixsuffix",
 				"test/contains/match",
-				// "test_contains_match", excluded by exclude filter
+				// "test_contains_match", excluded by excludeMetrics filter
 				"full/name/match",
 				"full_name_match",
 			}},
@@ -175,13 +178,13 @@ var (
 				{
 					"full_name_match",
 					"prefix/test/match",
-					// "prefix_test_match", excluded by exclude filter
+					// "prefix_test_match", excluded by excludeMetrics filter
 					"prefixprefix/test/match",
 					"test/match/suffix",
 					"test_match_suffix",
 					"test/match/suffixsuffix",
 					"test/contains/match",
-					// "test_contains_match", excluded by exclude filter
+					// "test_contains_match", excluded by excludeMetrics filter
 					"full/name/match",
 					"full_name_match",
 				},
@@ -292,6 +295,98 @@ var (
 		},
 	}
 )
+
+func TestFilterSpanProcessor(t *testing.T) {
+	factory := NewFactory()
+
+	includeFilter := &filterconfig.MatchProperties{
+		Config:    filterset.Config{MatchType: "regexp"},
+		SpanNames: []string{"^get prefix/.*", "^.*/suffix$"},
+	}
+
+	excludeFilter := &filterconfig.MatchProperties{
+		Config:    filterset.Config{MatchType: "regexp"},
+		SpanNames: []string{"^other_prefix/.*$", "^.*/other_suffix$"},
+	}
+
+	spanTests := []struct {
+		name              string
+		include           *filterconfig.MatchProperties
+		exclude           *filterconfig.MatchProperties
+		expectedSpanNames []string
+	}{
+		{
+			name:              "no filters",
+			include:           nil,
+			exclude:           nil,
+			expectedSpanNames: []string{"get prefix/foo", "other_prefix/foo", "bar"},
+		},
+		{
+			name:              "only include",
+			include:           includeFilter,
+			exclude:           nil,
+			expectedSpanNames: []string{"get prefix/foo"},
+		},
+		{
+			name:              "only exclude",
+			include:           nil,
+			exclude:           excludeFilter,
+			expectedSpanNames: []string{"get prefix/foo", "bar"},
+		},
+		{
+			name:              "include and exclude",
+			include:           includeFilter,
+			exclude:           excludeFilter,
+			expectedSpanNames: []string{"get prefix/foo"},
+		},
+	}
+
+	for _, test := range spanTests {
+		t.Run(test.name, func(t *testing.T) {
+			input := createTraces([]string{"get prefix/foo", "other_prefix/foo", "bar"})
+
+			cfg := &Config{
+				ProcessorSettings: config.NewProcessorSettings(config.NewID(typeStr)),
+				Spans: SpanFilters{
+					Include: test.include,
+					Exclude: test.exclude,
+				},
+			}
+
+			next := new(consumertest.TracesSink)
+			fmp, err := factory.CreateTracesProcessor(
+				context.Background(),
+				component.ProcessorCreateSettings{
+					Logger: zap.NewNop(),
+				},
+				cfg,
+				next,
+			)
+			assert.NotNil(t, fmp)
+			assert.Nil(t, err)
+
+			assert.NoError(t, fmp.Start(context.Background(), nil))
+
+			tErr := fmp.ConsumeTraces(context.Background(), input)
+			assert.NoError(t, tErr)
+			got := next.AllTraces()
+			assert.EqualValues(t, 1, len(got))
+			assert.EqualValues(t, createTraces(test.expectedSpanNames), got[0])
+		})
+	}
+}
+
+func createTraces(names []string) pdata.Traces {
+	td := pdata.NewTraces()
+	ils := td.ResourceSpans().AppendEmpty().InstrumentationLibrarySpans().AppendEmpty()
+
+	for _, name := range names {
+		span := ils.Spans().AppendEmpty()
+		span.SetName(name)
+	}
+
+	return td
+}
 
 func TestFilterMetricProcessor(t *testing.T) {
 	for _, test := range standardTests {
